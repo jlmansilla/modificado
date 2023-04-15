cf21f36bf57e22ca80b9e15c61d0e5caace94b6d
jlmansilla <juanmancillaster@gmail.com>
2023-04-14 22:08:14 -0400

    agrega faker en seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-email = "usuario2@gmail.com"
-password = "281ABASD932"
-User.create!(email: email, password: password, password_confirmation: password)
+require 'faker'
+ 
+User.create!(email: "admin@gmail.com", password: "291ABASD932", password_confirmation: "291ABASD932")
+User.create!(email: "usuario1@gmail.com", password: "291ABASD934", password_confirmation: "291ABASD934")
+User.create!(email: "usuario2@gmail.com", password: "281ABASD932", password_confirmation: "281ABASD932")
+
+25.times do
+    Lead.create([
+      {
+        email: Faker::Internet.email,
+        name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        phone: Faker::PhoneNumber.cell_phone_in_e164,
+        status: %w[prospecto interesado cliente][rand(3)],
+        user: User.all[rand(User.count)]
+      }
+    ])
